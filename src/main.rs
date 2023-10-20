8b5197e5eee84048dd1e282e080ed6c58ca15a18
Ambuj Singh <97109381+that-ambuj@users.noreply.github.com>
2023-10-20 14:49:43 +0530

    should work now?

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ use std::net::TcpListener;
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
+    let resp = "HTTP/1.1 200 OK\r\n\r\n";
+
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write_all("HTTP/1.1 200 OK\r\n".as_bytes()).unwrap();
+                stream.write_all(resp.as_bytes()).unwrap();
             }
             Err(e) => {
-                println!("error: {}", e);
+                eprintln!("error: {}", e);
             }
         }
     }
