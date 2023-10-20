216b1a5ec2f4fececa4aaa9c48ce4f6cd402e1b5
Ambuj Singh <97109381+that-ambuj@users.noreply.github.com>
2023-10-20 14:28:09 +0530

    stage 2: return 200 OK to all requests

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
+use std::io::Write;
 use std::net::TcpListener;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
