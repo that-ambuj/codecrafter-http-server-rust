296f2aece49115ab52d38749c90afd6e9079735a
Ambuj Singh <97109381+that-ambuj@users.noreply.github.com>
2023-10-20 15:01:11 +0530

    feat(stage 2): implement 404 not found response

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-use std::io::Write;
-use std::net::TcpListener;
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
-    let resp = "HTTP/1.1 200 OK\r\n\r\n";
-
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                stream.write_all(resp.as_bytes()).unwrap();
-            }
+            Ok(stream) => handle_stream(stream)?,
             Err(e) => {
                 eprintln!("error: {}", e);
             }
+        }
+    }
+
+    Ok(())
+}
+
+const OK_RESP: &'static str = "HTTP/1.1 200 OK\r\n\r\n";
+const NOT_FOUND_RESP: &'static str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+
+fn handle_stream(mut stream: TcpStream) -> std::io::Result<()> {
+    let reader = BufReader::new(&mut stream);
+
+    let mut lines = reader.lines();
+
+    if let Some(first_line) = lines.next() {
+        match first_line.as_deref() {
+            Ok("GET / HTTP/1.1") => stream.write_all(OK_RESP.as_bytes())?,
+            _ => stream.write_all(NOT_FOUND_RESP.as_bytes())?,
         }
     }
 
