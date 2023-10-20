aafd191cb504185bf3a6c6521948a8ac25285d51
Ambuj Singh <97109381+that-ambuj@users.noreply.github.com>
2023-10-20 23:05:33 +0530

    feat(stage 5): add parsing user-agent with GET /user-agent endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use nom::bytes::complete::{is_not, tag};
-use nom::sequence::delimited;
+use nom::character::complete::{char, multispace0, multispace1};
+use nom::sequence::{delimited, pair, preceded, separated_pair};
 use nom::IResult;
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
@@ -19,33 +20,75 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-const OK_RESP: &'static str = "HTTP/1.1 200 OK\r\n";
-const NOT_FOUND_RESP: &'static str = "HTTP/1.1 404 NOT FOUND\r\n";
+const OK_RESP: &str = "HTTP/1.1 200 OK\r\n";
+const NOT_FOUND_RESP: &str = "HTTP/1.1 404 NOT FOUND\r\n";
 
 fn handle_stream(mut stream: TcpStream) -> std::io::Result<()> {
     let reader = BufReader::new(&mut stream);
 
     let mut lines = reader.lines();
 
-    if let Some(first_line) = lines.next() {
+    let response = if let Some(first_line) = lines.next() {
         match first_line.as_deref() {
-            Ok("GET / HTTP/1.1") => Response::new_ok().write_tcp(&mut stream)?,
-            Ok(other) => match parse_echo_header(other) {
-                Ok((_, param)) => Response::new_ok().set_body(param).write_tcp(&mut stream)?,
-                e => {
-                    eprintln!("{:?}", e);
-                    Response::new_not_found().write_tcp(&mut stream)?
-                }
-            },
-            _ => Response::new_not_found().write_tcp(&mut stream)?,
+            Ok("GET / HTTP/1.1") => Response::new_ok(),
+            Ok(other) => parse_request(other),
+            _ => Response::new_not_found(),
         }
-    }
+    } else {
+        Response::new_not_found()
+    };
+
+    response.write_tcp(&mut stream)?;
 
     Ok(())
 }
 
+fn parse_request(input: &str) -> Response {
+    let mut lines = input.lines().filter(|l| !l.is_empty());
+
+    let path_header = lines.next().unwrap();
+
+    if let Ok((_, path)) = parse_path(path_header) {
+        match path {
+            "/user-agent" => {
+                if let Some(agent) = lines
+                    .filter_map(|l| parse_header_value(l).ok())
+                    .find(|(_, (h, _))| h == &"User-Agent")
+                    .map(|(_, (_, v))| v)
+                {
+                    Response::new_ok().set_body(agent)
+                } else {
+                    Response::new_not_found()
+                }
+            }
+            res if res.starts_with("/echo") => {
+                Response::new_ok().set_body(remove_echo_prefix(res).unwrap().1)
+            }
+            _ => Response::new_not_found(),
+        }
+    } else {
+        Response::new_not_found()
+    }
+}
+
+fn remove_echo_prefix(input: &str) -> IResult<&str, &str> {
+    preceded(tag("/echo/"), is_not(" "))(input)
+}
+
 fn parse_echo_header(input: &str) -> IResult<&str, &str> {
-    delimited(tag("GET /echo/"), is_not(" "), tag(" HTTP/1.1"))(input)
+    parse_path(input).and_then(|(_, res)| remove_echo_prefix(res))
+}
+
+fn parse_path(input: &str) -> IResult<&str, &str> {
+    delimited(
+        pair(tag("GET"), multispace1),
+        is_not(" "),
+        pair(multispace1, tag("HTTP/1.1")),
+    )(input)
+}
+
+fn parse_header_value(input: &str) -> IResult<&str, (&str, &str)> {
+    separated_pair(is_not(":"), pair(char(':'), multispace0), is_not(" "))(input)
 }
 
 #[derive(Default)]
@@ -131,5 +174,26 @@ mod tests {
             parse_echo_header("GET /echo/217/delta HTTP/1.1"),
             Ok(("", "217/delta"))
         );
+    }
+
+    #[test]
+    fn test_parse_path() {
+        assert_eq!(
+            parse_path("GET /hello/world HTTP/1.1"),
+            Ok(("", "/hello/world"))
+        );
+    }
+
+    #[test]
+    fn test_parse_header_value() {
+        assert_eq!(
+            parse_header_value("Content-Type: text/plain"),
+            Ok(("", ("Content-Type", "text/plain")))
+        );
+
+        assert_eq!(
+            parse_header_value("User-Agent: curl/7.64.1"),
+            Ok(("", ("User-Agent", "curl/7.64.1")))
+        )
     }
 }
